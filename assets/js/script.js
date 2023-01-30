02ba44f035bbd23c0fe7d4caa8c166f70008ffe7
Igor Oliveira de Sousa <igor.oliveira@rfsolucoes.local>
2023-01-30 17:49:33 -0300

    ajustando js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
+let slider = document.querySelector('.slider').clientHeight;
 let totalSliders = document.querySelectorAll('.slider-item').length;
 let sliderContainer = document.querySelector('.slider-container').style.width;
-let sliderControlsH = document.querySelector('.slider-controls');
-let slider = document.querySelector('.slider').clientHeight;
+let sliderControls = document.querySelector('.slider-controls');
+
 let currentSlide = 0;
 
 sliderContainer = `calc(100vw * ${totalSliders})`;
-sliderControlsH.style.height = `${slider}px`;
+sliderControls.style.height = `${slider}px`;
 
 function goPrev() {
     currentSlide--;
@@ -26,10 +27,7 @@ function goNext() {
 function updateMargin() {
     let sliderItemWidth = document.querySelector('.slider-item').clientWidth;
     let sliderWidth = document.querySelector('.slider-item');
-    let maxWidth = document.body.clientWidth;
     let newMargin = (currentSlide * sliderItemWidth);
 
     sliderWidth.style.marginLeft = `-${newMargin}px`;
 }
-
-setInterval(goNext, 5000);
